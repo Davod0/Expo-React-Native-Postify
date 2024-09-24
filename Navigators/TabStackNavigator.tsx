26db77d53bd90818107754c0c4f9b272235c179d
Davod <davodnikoyii@gmail.com>
2024-09-24 12:50:10 +0200

    Implemented a new navigator (CreatePost) and its screen (CreatePostScreen)

--- Navigators/TabStackNavigator.tsx
+++ Navigators/TabStackNavigator.tsx
@@ -1,12 +1,14 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
+import { Text } from "react-native";
+import CreatePostScreen from '../Screens/CreatePostScreen';
 import CreateUserScreen from '../Screens/CreateUserScreen';
 import FavoriteScreen from "../Screens/FavoritePostsScreen";
 import StartPageScreen from "../Screens/StartPageScreen";
-import {Text} from "react-native";
 
 export type TabStackParamList = {
     FavoritePosts: undefined;
@@ -25,38 +27,53 @@ export default function TabStackNavigator(){
     return (
         <TabStack.Navigator
            screenOptions= {({navigation}) => ({headerRight: (props) => (
-                <SimpleLineIcons name="user" size={24} color="black" 
-                style={{ marginRight: 16, fontSize:30, color:"#90EE90" }}
-                onPress={() => navigation.navigate("UserAccount", {userId: userId, userName: userName})}
-                />
+                <SimpleLineIcons name="user" size={24}  
+                style={{ marginRight: 16, fontSize:30, color:"#B0BEC5" }}
+                onPress={() => navigation.navigate("UserAccount", {userId: userId, userName: userName})}/>
                 )})
-            }
-            >
+            }>
             <TabStack.Screen name="StartPage" component={StartPageScreen} 
                options={{
-                    tabBarIcon: (props) => <MaterialIcons name="home" size={24} color={props.color} />,
-                    title: "All Posts",
+                   title: "All Posts",
+                   tabBarIcon: (props) => <MaterialIcons name="home" size={24} color={props.focused? "#90EE90" : "#B0BEC5"} />,
+                   tabBarLabel: ({ focused }) => (
+                    <Text style={{ color: focused ? "#90EE90" : "#B0BEC5", fontSize: 12 }}>
+                        All Posts
+                    </Text>)
                 }}
             />
+            <TabStack.Screen name="CreatePost" component={CreatePostScreen} 
+                options={{
+                    title: "Create New Post",
+                    tabBarIcon: (props) => <Ionicons name="create" size={24} color={props.focused? "#90EE90" : "#B0BEC5"}/>,
+                    tabBarLabel: ({ focused }) => (
+                    <Text style={{ color: focused ? "#90EE90" : "#B0BEC5", fontSize: 12 }}>
+                        Create New Post
+                    </Text>)
+                }}
+            />   
             <TabStack.Screen name="FavoritePosts" component={FavoriteScreen}
                options={{
-                    tabBarIcon: (props) => <MaterialIcons name="favorite" size={24} color={props.color} />,
-                    title: "Favorite Posts",
+                   title: "Favorite Posts",
+                   tabBarIcon: (props) => <MaterialIcons name="favorite" size={24} color={props.focused? "#90EE90" : "#B0BEC5"} />,
+                   tabBarLabel: ({ focused }) => (
+                    <Text style={{ color: focused ? "#90EE90" : "#B0BEC5", fontSize: 12 }}>
+                        Favorite Posts
+                    </Text>)
                 }} 
-                />
-
+            />
             <TabStack.Screen name="CreateUser" component={CreateUserScreen} 
                 options={{
                     title: "Create New User",
-                    tabBarIcon: (props) => <FontAwesome name="user-plus" size={24} color={props.focused? "#87CEFA" : "#B0BEC5"}/>,
+                    tabBarIcon: (props) => <FontAwesome name="user-plus" size={24} color={props.focused? "#90EE90" : "#B0BEC5"}/>,
                     tabBarLabel: ({ focused }) => (
-                    <Text style={{ color: focused ? "#ADD8E6" : "#B0BEC5", fontSize: 12 }}>
+                    <Text style={{ color: focused ? "#90EE90" : "#B0BEC5", fontSize: 12 }}>
                         Create New User
-                    </Text>),
-                    }}/>   
+                    </Text>)
+                }}
+            />   
         </TabStack.Navigator>
     );
 };
 
 
-// tabBarIcon: () => <Ionicons name="create" size={24} color="black" />
