42e4fead364c43da28653124a0fd1addde7489d5
Davod <davodnikoyii@gmail.com>
2024-09-24 12:17:36 +0200

    Implemented a new navigator  (CreateUser) and a new screen (CreateUserScreeen)

--- Navigators/TabStackNavigator.tsx
+++ Navigators/TabStackNavigator.tsx
@@ -1,13 +1,18 @@
 import { MaterialIcons } from '@expo/vector-icons';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
+import CreateUserScreen from '../Screens/CreateUserScreen';
 import FavoriteScreen from "../Screens/FavoritePostsScreen";
 import StartPageScreen from "../Screens/StartPageScreen";
+import {Text} from "react-native";
 
 export type TabStackParamList = {
     FavoritePosts: undefined;
     StartPage: undefined;
+    CreateUser : undefined;
+    CreatePost: undefined;
 }
 
 const TabStack = createBottomTabNavigator<TabStackParamList>();
@@ -20,9 +25,9 @@ export default function TabStackNavigator(){
     return (
         <TabStack.Navigator
            screenOptions= {({navigation}) => ({headerRight: (props) => (
-                <FontAwesome5 name="user"
+                <SimpleLineIcons name="user" size={24} color="black" 
                 style={{ marginRight: 16, fontSize:30, color:"#90EE90" }}
-                 onPress={() => navigation.navigate("UserAccount", {userId: userId, userName: userName})}
+                onPress={() => navigation.navigate("UserAccount", {userId: userId, userName: userName})}
                 />
                 )})
             }
@@ -30,13 +35,28 @@ export default function TabStackNavigator(){
             <TabStack.Screen name="StartPage" component={StartPageScreen} 
                options={{
                     tabBarIcon: (props) => <MaterialIcons name="home" size={24} color={props.color} />,
+                    title: "All Posts",
                 }}
             />
             <TabStack.Screen name="FavoritePosts" component={FavoriteScreen}
-             options={{
-                 tabBarIcon: (props) => <MaterialIcons name="favorite" size={24} color={props.color} />,
+               options={{
+                    tabBarIcon: (props) => <MaterialIcons name="favorite" size={24} color={props.color} />,
+                    title: "Favorite Posts",
                 }} 
                 />
+
+            <TabStack.Screen name="CreateUser" component={CreateUserScreen} 
+                options={{
+                    title: "Create New User",
+                    tabBarIcon: (props) => <FontAwesome name="user-plus" size={24} color={props.focused? "#87CEFA" : "#B0BEC5"}/>,
+                    tabBarLabel: ({ focused }) => (
+                    <Text style={{ color: focused ? "#ADD8E6" : "#B0BEC5", fontSize: 12 }}>
+                        Create New User
+                    </Text>),
+                    }}/>   
         </TabStack.Navigator>
     );
 };
+
+
+// tabBarIcon: () => <Ionicons name="create" size={24} color="black" />
---
+++ Screens/CreateUserScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+
+export default function CreateUserScreen(){
+    return(
+        <View>
+            <Text>Create User Screen</Text>
+        </View>
+    );
+}
