3ba7062ee281b918ed1ef5dc3fce00110bb33786
Davod <davodnikoyii@gmail.com>
2024-09-25 08:18:06 +0200

    Created a type for a user to send to a server, created a method to generate a unique ID

--- Actions/actions.ts
+++ Actions/actions.ts
@@ -1,6 +1,20 @@
 import { User } from "../Components/User/UserSchema";
+import { IUser } from "../data";
+
+function CreateUniqueId() {
+    return Date.now().toString(36) + Math.random().toString(36);
+}
 
 export async function AddUserToServer(data: User) {
-    // return await axios.post('/api/users',data)
-    console.log("From data:", data)
-} 
+
+    const id = CreateUniqueId();
+    const user: IUser = {
+        id: id,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password,
+    }
+    console.log("User:", user)
+    // await = axios.post("http://localhost:3000/users", user);
+}
