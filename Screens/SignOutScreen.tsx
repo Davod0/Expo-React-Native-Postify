072692f41af23b696f12670842834ecfce3fbf09
Davod <davodnikoyii@gmail.com>
2024-10-03 21:24:21 +0200

    Added styling for SignOutScreen

--- Screens/SignOutScreen.tsx
+++ Screens/SignOutScreen.tsx
@@ -1,7 +1,8 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Button, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { useUser } from "../Components/User/UserProvider";
 import { TabStackParamList } from "../Navigators/TabStackNavigator";
+import { Button } from "react-native-paper";
 
 type Props = NativeStackScreenProps<TabStackParamList, "SignOut">;
 
@@ -9,15 +10,45 @@ export default function SignOutScreen(props: Props) {
   const { setCurrentUser } = useUser();
 
   return (
-    <View>
-      <Text>SignOutScreen</Text>
+    <View style={styles.authContainer}>
+      <Text style={styles.authText}>Click On This Button To Sign Out</Text>
 
       <Button
-        title="SignOut"
-        onPress={() => {
-          setCurrentUser(null);
-        }}
-      />
+        mode="contained"
+        onPress={() => setCurrentUser(null)}
+        style={styles.button}
+        labelStyle={styles.buttonLabel}
+      >
+        Sign Out
+      </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  authContainer: {
+    alignItems: "center",
+    padding: 20,
+  },
+  authText: {
+    fontSize: 18,
+    marginBottom: 20,
+    color: "#333",
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    backgroundColor: "#6200ea",
+    borderRadius: 25,
+  },
+  buttonLabel: {
+    fontSize: 16,
+    color: "#fff",
+  },
+});
